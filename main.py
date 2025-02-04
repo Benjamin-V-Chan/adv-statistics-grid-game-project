d74d2d6486d83b6114ed54a6d9c6a5bf0b4c69e6
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-02-03 22:03:48 -0800

    CONSTANTS section and implementation

--- main.py
+++ main.py
@@ -7,19 +7,39 @@ screen_size = 800
 display = pygame.display.set_mode((screen_size, screen_size))
 clock = pygame.time.Clock()
 
+# Constants
+WHITE = (255, 255, 255)
+LIGHT_GREY = (200, 200, 200)
+BLACK = (0, 0, 0)
+RED = (255, 64, 64)
+BLUE = (64, 64, 255)
+
+TILE_STATUS_COLORS = {
+    "empty": WHITE,
+    "mouse_hover": LIGHT_GREY,
+    "player1": RED,
+    "player2": BLUE
+}
+
+FPS = 60
+
 # Main Loop
 def main():
-
     running = True
 
     while running:
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Rendering
+        display.fill(BLACK)
+
         # Pygame updates
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(FPS)
 
     pygame.quit()
 
